5f06f66b1ccc216e3a72e54ab2d85a8219f14caa
Esmaofcj <felcrisjeneesmao@gmail.com>
2023-11-11 09:03:21 +0800

    added my first commit

---
+++ src/Parent.java
@@ -0,0 +1,16 @@
+public class Parent {
+    String name;
+    
+    Parent(String name){
+        this.name = name;
+    }
+    void kapoy(){
+        System.out.println(name + " kaya rani, laban lang");
+    }
+    void hatdog(){
+        System.out.println(name + " it's better to be lonely");
+    }
+
+    
+    
+}
